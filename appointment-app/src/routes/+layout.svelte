d3ac1f4d92469d2dfbc79acf689ca8a7d4154710
Sahil Dinanath <sahildinanath@gmail.com>
2023-03-20 09:48:58 +0200

    added navbar and schedule

--- appointment-app/src/routes/+layout.svelte
+++ appointment-app/src/routes/+layout.svelte
@@ -2,23 +2,33 @@
   import '../app.css';
 </script>
 
-<html data-theme="corporate"></html>
-<!--Creates Sidebar-->
-<div class="drawer drawer-mobile">
-  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content flex flex-col items-center justify-center">
-    <!-- Page content here -->
-    <slot />
-    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+<html data-theme="autumn"></html>
+
+<div class="navbar bg-base-100">
+  <div class="flex-none">
+   <div class="navbar-start">
+    <div class="dropdown">
+      <label tabindex="0" class="btn btn-ghost btn-square">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      </label>
+      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Profile</a></li>
+        <li><a>Schedule</a></li>
+        <li><a>About</a></li>
+      </ul>
+    </div>
+  </div>
+  </div>
   
-  </div> 
-  <div class="drawer-side">
-    <label for="my-drawer-2" class="drawer-overlay"></label> 
-    <ul class="menu p-4 w-80 bg-base-100 text-base-content">
-      <!-- Sidebar content here -->
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-    </ul>
-  
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">Book2Meet</a>
+  </div>
+  <div class="flex-none">
+    <button class="btn btn-square btn-ghost">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+      
+    </button>
+    
   </div>
 </div>
+<slot/>
