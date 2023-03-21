07ed2dacdcc376033f9b3f85f0249a6bcc22c954
Sahil Dinanath <sahildinanath@gmail.com>
2023-03-21 18:04:45 +0200

    removed content of createEvents() function

--- appointment-app/src/routes/schedule/+page.svelte
+++ appointment-app/src/routes/schedule/+page.svelte
@@ -4,33 +4,23 @@
     import '@event-calendar/core/index.css';
     import Interaction from '@event-calendar/interaction'
 
-    let text;
+
     let plugins = [TimeGrid,Interaction];
     let options = {
         view: 'timeGridWeek',
         allDaySlot: false,
         pointer: true,
         selectable: true,
+        unselectAuto: false,
         nowIndicator: true,
         headerToolbar: { start: 'prev,next today', center: 'title', end:''},
         events: createEvents(),
     };
     function createEvents() { 
-        let days = []; 
-        for (let i = 0; i < 7; ++i) { 
-	        let day = new Date(); 
-	        let diff = i - day.getDay(); 
-	        day.setDate(day.getDate() + diff); 
-	        days[i] = day.getFullYear() + "-" + _pad(day.getMonth()+1) + "-" +_pad(day.getDate()); 
-	    }  
-        return [
-        {
-        start: days[5] + " 18:00", 
-        end: days[5] + " 21:00", 
-        resourceId: 2, 
-        title: "", 
-        color: "#B29DD9"}, 
-        ]; 
+        
+    }
+    function setEvent(){
+
     }
 
     function _pad(num) { 
