ca57d8d69942fd1d04c0bcf094e6c73c77d3189e
GyuoMi <darshnsingh10@gmail.com>
2023-03-24 22:13:01 +0200

    fixing firebase library

--- appointment-app/src/auth/index.js
+++ appointment-app/src/auth/index.js
@@ -1,32 +1,25 @@
 import firebase from 'firebase/app'
-import initializeApp from 'firebase/app';
-import  from 'firebase/auth';
-import { firebaseConfig } from "./firebaseConfig.js";
+import 'firebase/auth'
+import { firebaseConfig } from './firebaseConfig.js'
+import { readable } from 'svelte/store'
+firebase.initializeApp(firebaseConfig)
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+//authentication
+export const initAuth = () => {
+  const auth  = firebase.auth()
 
-// Get a reference to the authentication service
-export const auth = getAuth(app);
-export default app
-// Set up Google authentication
-const googleProvider = new GoogleAuthProvider();
+  // Using a redirect.
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential instanceof firebase.auth.OAuthCredential) {
+    // This gives you a Google Access Token.
+    var token = result.credential.accessToken;
+  }
+  var user = result.user;
+});
 
-signInWithPopup(auth, googleProvider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+// Start a sign in process for an unauthenticated user.
+var provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('profile');
+provider.addScope('email');
+firebase.auth().signInWithRedirect(provider);
+}
