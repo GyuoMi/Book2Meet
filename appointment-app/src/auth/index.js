7f26b625e4af749ae976c56ab79d8f7fae8a10a0
GyuoMi <darshnsingh10@gmail.com>
2023-03-24 23:18:52 +0200

    login component testing

--- appointment-app/src/auth/index.js
+++ appointment-app/src/auth/index.js
@@ -8,18 +8,25 @@ firebase.initializeApp(firebaseConfig)
 export const initAuth = () => {
   const auth  = firebase.auth()
 
-  // Using a redirect.
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential instanceof firebase.auth.OAuthCredential) {
-    // This gives you a Google Access Token.
-    var token = result.credential.accessToken;
-  }
-  var user = result.user;
-});
+  const loginWithGoogle = () => {
+    // Using a redirect.
+  firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential instanceof firebase.auth.OAuthCredential) {
+      // This gives you a Google Access Token.
+      var token = result.credential.accessToken;
+    }
+    var user = result.user;
+  });
+  
+  // Start a sign in process for an unauthenticated user.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('profile');
+  provider.addScope('email');
+  firebase.auth().signInWithRedirect(provider);
 
-// Start a sign in process for an unauthenticated user.
-var provider = new firebase.auth.GoogleAuthProvider();
-provider.addScope('profile');
-provider.addScope('email');
-firebase.auth().signInWithRedirect(provider);
+  
+  return{
+    loginWithGoogle
+  }
+  }
 }
