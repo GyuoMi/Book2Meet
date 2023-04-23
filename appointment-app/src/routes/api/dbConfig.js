129e52a10ced5ecae2a3288020f663f804700deb
GyuoMi <darshnsingh10@gmail.com>
2023-04-23 20:41:35 +0200

    added ssh2 from npm and created dbConfig as a temp for database.js

---
+++ appointment-app/src/routes/api/dbConfig.js
@@ -0,0 +1,24 @@
+import mysql from 'mysql2/promise'
+const { Client } = require('ssh2');
+import Client from 'ssh2'
+
+// define connection config for the database
+const dbServer = {
+    host: 'localhost',
+    user: 'db_user',
+    password: 'db_user',
+    database: 'Book2Meet'
+}
+// define connection config for the ssh tunnel
+const tunnelConfig = {
+    host: '10.100.15.77',
+    port: 22,
+    username: 'student',
+    password: 'ohx4cung'
+}
+const forwardConfig = {
+    srcHost: '127.0.0.1', // any valid address
+    srcPort: 3306, // any valid port
+    dstHost: dbServer.host, // destination database
+    dstPort: dbServer.port // destination port
+};
