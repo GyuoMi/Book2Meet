0dfad8c7f66d85933356fcd4b0327ad502400496
Sahil Dinanath <sahildinanath@gmail.com>
2023-04-29 19:21:39 +0200

    users can now search for other users schedule

---
+++ appointment-app/src/routes/appointments/+page.server.js
@@ -0,0 +1,16 @@
+import database from '../api/database.js'
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+  getSearchedEmailEvents: async ({request})=>{
+    const responseData = await request.formData();
+    const email = responseData.get('email');
+    let clientEvents = await database.getJsonFromSelectQuery(
+      `Select EVENT_ID,EVENT_START,EVENT_END,EVENT_TITLE,EVENT_COLOR FROM EVENT_TBL RIGHT JOIN CLIENT_TBL ON EVENT_TBL.CLIENT_ID = CLIENT_TBL.CLIENT_ID WHERE CLIENT_TBL.CLIENT_EMAIL = "${email}"`);
+    console.log(clientEvents);
+    return{
+      success: true,
+      post: clientEvents
+    };
+  }
+}
---
+++ appointment-app/src/routes/appointments/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Calendar from '@event-calendar/core';
+	import TimeGrid from '@event-calendar/time-grid';
+	import '@event-calendar/core/index.css';
+	import Interaction from '@event-calendar/interaction';
+	import { getArrayOfEventsFromDatabase } from '../schedule/page.modules';
+ /** @type {import('./$types').ActionData} */    
+  export let form;
+
+  let eventsFromClient;
+  let ec;
+  let plugins = [TimeGrid, Interaction];
+	let options = {
+		view: 'timeGridWeek',
+		allDaySlot: false,
+		pointer: true,
+		selectable: true,
+		unselectAuto: true,
+		nowIndicator: true,
+		height: '800px',
+		headerToolbar: { start: 'prev,next today', center: 'title', end: '' },
+    events: getArrayOfEventsFromDatabase(form)
+  }
+   
+</script>
+
+<form method="POST" action="?/getSearchedEmailEvents">
+<div align="center">
+  <input name="email" type="text" placeholder="search by email" class="input input-bordered input-primary w-100" />
+  <button class="btn btn-primary">Button</button>
+</div>
+</form>
+<Calendar bind:this={ec} {plugins} {options} />
