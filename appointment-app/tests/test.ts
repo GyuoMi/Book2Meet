57b22021cb0578c530ab860af9467db2eca7f67f
Sahil Dinanath <sahildinanath@gmail.com>
2023-04-29 11:25:12 +0200

    removed white space

--- appointment-app/tests/test.ts
+++ appointment-app/tests/test.ts
@@ -1,6 +1,5 @@
 import { expect, test } from '@playwright/test';
 
-
 test('homepage expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'Ready to Book an Appointment?' })).toBeVisible();
