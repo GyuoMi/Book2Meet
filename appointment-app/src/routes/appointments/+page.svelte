5ef9179b17b704f0c4367464310d141044a8d82a
Sahil Dinanath <sahildinanath@gmail.com>
2023-04-30 16:29:19 +0200

    added deleting of events

--- appointment-app/src/routes/appointments/+page.svelte
+++ appointment-app/src/routes/appointments/+page.svelte
@@ -3,13 +3,13 @@
 	import TimeGrid from '@event-calendar/time-grid';
 	import '@event-calendar/core/index.css';
 	import Interaction from '@event-calendar/interaction';
-	import { getArrayOfEventsFromDatabase } from '../schedule/page.modules';
+	import { deleteEventFromCalender, getArrayOfEventsFromDatabase } from '../schedule/page.modules';
 	/** @type {import('./$types').ActionData} */
 	export let form;
 
 	let userBookings = [];
 	let clientEvents = getArrayOfEventsFromDatabase(form);
-
+	let currentEventSelected;
 	let ec;
 	let plugins = [TimeGrid, Interaction];
 	let options = {
@@ -24,17 +24,10 @@
 		events: clientEvents,
 		eventDurationEditable: false,
 		eventStartEditable: false,
-		select: createEventWithPointer
+		select: createEventWithPointer,
+    eventClick: getCurrentEventClicked
+
 	};
-	//ensures that the user can only delete their own booking
-	function isUserBooking(selectedId) {
-		for (let i = 0; i < clientEvents.length; i++) {
-			if (clientEvents[i].id == selectedId) {
-				return false;
-			}
-		}
-		return true;
-	}
 	//checks to see if user made a booking inside a client availibility window
 	function isUserBookingValid(event) {
 		for (let i = 0; i < clientEvents.length; i++) {
@@ -43,7 +36,7 @@
 			//!!!!!!tempory removing 2 hours to dates for now
 			startDate.setHours(startDate.getHours() - 2);
 			endDate.setHours(endDate.getHours() - 2);
-      //fix
+			//fix
 			let startEventDate = new Date(event.start);
 			let endEventDate = new Date(event.end);
 			if (startDate <= startEventDate && endDate >= endEventDate) {
@@ -61,7 +54,24 @@
 			end: event.end,
 			title: 'booking'
 		});
-		// userBookings.push(event.)
+		userBookings.push(event);
+	}
+
+	//ensures that the user can only delete their own booking
+  //Todo fix this
+	function isUserBooking(event) {
+		for (let i = 0; i < userBookings.length; i++) {
+			if (userBookings[i].id == event.id) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function getCurrentEventClicked(event) {
+		// if (isUserBooking(event.event)) {
+			currentEventSelected = event;
+		// }
 	}
 </script>
 
@@ -77,3 +87,10 @@
 	</div>
 </form>
 <Calendar bind:this={ec} {plugins} {options} />
+
+<div class="flex flex-col items-center">
+	<button
+		on:click={deleteEventFromCalender(ec, currentEventSelected)}
+		class="btn btn-primary place-item-center">Delete Event</button
+	>
+</div>
