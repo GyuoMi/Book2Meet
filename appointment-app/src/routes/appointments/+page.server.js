00cc7abf239c3a2e6f131b65e8993400ea221471
Sahil Dinanath <sahildinanath@gmail.com>
2023-05-01 12:21:43 +0200

    backend implementation to allow fetching of bookings a user made to a client

--- appointment-app/src/routes/appointments/+page.server.js
+++ appointment-app/src/routes/appointments/+page.server.js
@@ -2,14 +2,19 @@ import database from '../api/database.js'
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-  getSearchedEmailEvents: async ({request})=>{
+  getSearchedEmailEvents: async ({ request }) => {
     const responseData = await request.formData();
     const email = responseData.get('email');
     let clientEvents = await database.getJsonFromSelectQuery(
       `Select EVENT_ID,EVENT_START,EVENT_END,EVENT_TITLE,EVENT_COLOR FROM EVENT_TBL RIGHT JOIN CLIENT_TBL ON EVENT_TBL.CLIENT_ID = CLIENT_TBL.CLIENT_ID WHERE CLIENT_TBL.CLIENT_EMAIL = "${email}"`);
-    return{
+    let userBookings = await database.getJsonFromSelectQuery('Select EVENT_TBL.EVENT_ID,EVENT_TBL.EVENT_START,EVENT_TBL.EVENT_END,EVENT_TBL.EVENT_TITLE,EVENT_TBL.EVENT_COLOR FROM BOOKING_TBL LEFT JOIN EVENT_TBL ON BOOKING_TBL.EVENT_ID = EVENT_TBL.EVENT_ID');
+    let allEvents = {
+      results: clientEvents.results.concat(userBookings.results)
+    };
+    console.log(allEvents);
+    return {
       success: true,
-      post: clientEvents
+      post: allEvents,
     };
   }
 }
