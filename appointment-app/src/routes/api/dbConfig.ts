97bfe7a2c830d262e4dcc9ed57683ae827a9afc4
GyuoMi <darshnsingh10@gmail.com>
2023-05-04 11:20:29 +0200

    added test cases for signup page Installed 2 new packages for testing check discord soon

--- appointment-app/src/routes/api/dbConfig.ts
+++ appointment-app/src/routes/api/dbConfig.ts
@@ -1,62 +1,96 @@
 import mysql from 'mysql2/promise';
-import tunnel from 'tunnel-ssh';
+import { Client } from 'ssh2';
 
 const sshConfig = {
-    host: 'connect.wits.ac.za',
-    username: 'student',
-    password: 'ohx4cung',
-    dstHost: '127.0.0.1',
-    dstPort: 3306, // Default MySQL port
-    localHost: '127.0.0.1',
-    localPort: 3307, // Any unused port
+  host: 'connect.wits.ac.za',
+  username: 'student',
+  password: 'ohx4cung',
+  dstHost: '127.0.0.1',
+  dstPort: 3306, // Default MySQL port
+  localHost: '127.0.0.1',
+  localPort: 3307, // Any unused port
 };
 
 const mysqlConfig = {
-    host: '127.0.0.1',
-    user: 'db_user',
-    password: 'db_user',
-    database: 'Book2Meet',
-    port: 3307, // Port opened by SSH tunnel
+  host: '127.0.0.1',
+  user: 'db_user',
+  password: 'db_user',
+  database: 'Book2Meet',
+  port: 3307, // Port opened by SSH tunnel
 };
 
-let tunnelConfig = {
-    ...sshConfig,
-    keepAlive: true,
-    keepAliveInterval: 10000,
-};
+let sshTunnel: Client | null = null;
 
 const createConnectionPool = async () => {
-    try {
-        const server = await tunnel(tunnelConfig);
-        const connectionPool = mysql.createPool(mysqlConfig);
-        console.log(`Connected to MySQL server through SSH tunnel`);
-        return connectionPool;
-    } catch (err) {
-        console.error(`Error establishing SSH tunnel: ${err.message}`);
-        process.exit(1);
-    }
+  try {
+    sshTunnel = new Client();
+    await new Promise<void>((resolve, reject) => {
+      sshTunnel!.on('ready', () => {
+        sshTunnel!.forwardOut(
+          sshConfig.localHost,
+          sshConfig.localPort,
+          sshConfig.dstHost,
+          sshConfig.dstPort,
+          (err, stream) => {
+            if (err) {
+              reject(err);
+            } else {
+              const connection = mysql.createConnection({
+                ...mysqlConfig,
+                stream,
+              });
+              connection.connect((err) => {
+                if (err) {
+                  reject(err);
+                } else {
+                  console.log('Connected to MySQL server through SSH tunnel');
+                  resolve();
+                }
+              });
+            }
+          }
+        );
+      });
+      sshTunnel!.on('error', (err) => {
+        reject(err);
+      });
+      sshTunnel!.on('close', () => {
+        console.log('SSH tunnel closed');
+      });
+      sshTunnel!.connect(sshConfig);
+    });
+    const connectionPool = mysql.createPool(mysqlConfig);
+    console.log(`Connected to MySQL server through SSH tunnel`);
+    return connectionPool;
+  } catch (err) {
+    console.error(`Error establishing SSH tunnel: ${err.message}`);
+    process.exit(1);
+  }
 };
 
 const mysqlconn = await createConnectionPool();
 
-export async function getJsonFromSelectQuery(query) {
-	let results = await mysqlconn.query(query).then(function ([rows, fields]) {
-		return rows;
-	});
-
-	return {
-		results
-	};
+export async function getJsonFromSelectQuery(query: string) {
+  const [rows, fields] = await mysqlconn.query(query);
+  return { results: rows };
 }
 
-export async function changeDataInDatabase(query) {
-	await mysqlconn.query(query);
+export async function changeDataInDatabase(query: string) {
+  await mysqlconn.query(query);
 }
+
+process.on('SIGINT', () => {
+  console.log('Closing SSH tunnel and MySQL connection pool');
+  if (sshTunnel) {
+    sshTunnel.end();
+  }
+  mysqlconn.end();
+  process.exit();
+});
 
 export default {
-	changeDataInDatabase,
-	getJsonFromSelectQuery,
-	mysqlconn
+  changeDataInDatabase,
+  getJsonFromSelectQuery,
+  mysqlconn,
 };
-
 
---
+++ appointment-app/src/routes/signup/signup.test.ts
@@ -0,0 +1,77 @@
+import { fireEvent, render, screen, expect } from "@testing-library/svelte";
+import "@testing-library/jest/extend-expect";
+// import { expect } from 'vitest';
+import userEvent from "@testing-library/user-event";
+import SignupForm from "./+page.svelte";
+
+describe("SignupForm", () => {
+  beforeEach(() => {
+    render(SignupForm);
+  });
+
+  it("should show error message when first name input is empty", async () => {
+    const input = screen.getByPlaceholderText("John");
+    await userEvent.clear(input);
+    await userEvent.tab();
+    expect(screen.getByText("Please fill in all the required fields")).toBeInTheDocument();
+  });
+
+  it("should show error message when email input is invalid", async () => {
+    const emailInput = screen.getByPlaceholderText("Email");
+    await userEvent.type(emailInput, "invalidemail");
+    await userEvent.tab();
+    expect(screen.getByText("Please enter a valid email address.")).toBeInTheDocument();
+  });
+
+  it("should show error message when password input is invalid", async () => {
+    const passwordInput = screen.getByPlaceholderText("Password");
+    await userEvent.type(passwordInput, "weak");
+    await userEvent.tab();
+    expect(screen.getByText("Password must be at least 8 characters long")).toBeInTheDocument();
+  });
+
+  it("should show error message when confirm password input does not match password", async () => {
+    const passwordInput = screen.getByPlaceholderText("Password");
+    const confirmPasswordInput = screen.getByPlaceholderText("Confirm Password");
+    await userEvent.type(passwordInput, "validPassword@123");
+    await userEvent.type(confirmPasswordInput, "invalidPassword@123");
+    await userEvent.tab();
+    expect(screen.getByText("Passwords do not match.")).toBeInTheDocument();
+  });
+
+  it("should enable submit button when all inputs are valid", async () => {
+    const firstNameInput = screen.getByPlaceholderText("John");
+    const lastNameInput = screen.getByPlaceholderText("Snow");
+    const emailInput = screen.getByPlaceholderText("Email");
+    const passwordInput = screen.getByPlaceholderText("Password");
+    const confirmPasswordInput = screen.getByPlaceholderText("Confirm Password");
+    await userEvent.type(firstNameInput, "John");
+    await userEvent.type(lastNameInput, "Snow");
+    await userEvent.type(emailInput, "john.snow@example.com");
+    await userEvent.type(passwordInput, "validPassword@123");
+    await userEvent.type(confirmPasswordInput, "validPassword@123");
+    expect(screen.getByRole("button", { name: "Sign up" })).not.toBeDisabled();
+  });
+
+  it("should disable submit button when any input is invalid", async () => {
+    const firstNameInput = screen.getByPlaceholderText("John");
+    const lastNameInput = screen.getByPlaceholderText("Snow");
+    const emailInput = screen.getByPlaceholderText("Email");
+    const passwordInput = screen.getByPlaceholderText("Password");
+    const confirmPasswordInput = screen.getByPlaceholderText("Confirm Password");
+    await userEvent.type(firstNameInput, "John");
+    await userEvent.type(lastNameInput, "Snow");
+    await userEvent.type(emailInput, "invalidemail");
+    await userEvent.type(passwordInput, "weak");
+    await userEvent.type(confirmPasswordInput, "weak");
+    expect(screen.getByRole("button", { name: "Sign up" })).toBeDisabled();
+  });
+
+  it("should submit form when all inputs are valid", async () => {
+    const firstNameInput = screen.getByPlaceholderText("John");
+    const lastNameInput = screen.getByPlaceholderText("Snow");
+    const emailInput = screen.getByPlaceholderText("Email");
+    const passwordInput = screen.getByPlaceholderText("Password");
+    const confirmPasswordInput = screen.getByPlaceholderText("Confirm Password");
+  }); 
+});
