65863e77aee47188fd490a6b13850b973bc20e2d
Ilona Smith <smithilona247@gmail.com>
2023-05-07 20:19:58 +0200

    changed bell icon nav bar

--- appointment-app/src/routes/+layout.svelte
+++ appointment-app/src/routes/+layout.svelte
@@ -50,21 +50,13 @@
 			/>
 		</a>
 	</div>
-	<div class="flex-none">
-		<button class="btn btn-square btn-ghost">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				class="inline-block w-5 h-5 stroke-current"
-				><path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-				/></svg
-			>
-		</button>
-	</div>
+  <div class="dropdown dropdown-end">
+    <button ><i class='far fa-bell' style='font-size:24px'></i>
+    </button>
+    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+      <li><a>Item 1</a></li>
+      <li><a>Item 2</a></li>
+    </ul>
+  </div>
 </div>
 <slot />
