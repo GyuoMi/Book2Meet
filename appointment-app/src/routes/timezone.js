0efa2ec71722917fe6964f1c0b68edddddf8d69a
Sahil Dinanath <sahildinanath@gmail.com>
2023-05-10 18:22:45 +0200

    added function to convert to different timezones

--- appointment-app/src/routes/timezone.js
+++ appointment-app/src/routes/timezone.js
@@ -1,2 +1,16 @@
+import { DateTime } from "luxon";
 
+export function convertDateToClientTimezone(uctTimeFromDb,clientTimezone){
+  let uctTime = DateTime.fromISO(uctTimeFromDb);
+  let rezonedTime = uctTime.setZone(clientTimezone);
+  return rezonedTime.toString();
+}
+
+export function convertTimezoneOfEventList(eventList, clientTimezone) {
+  for (let i = 0; i < eventList.results.length; i++) {
+    eventList.results[i].EVENT_START = convertDateToClientTimezone(eventList.results[i].EVENT_START, clientTimezone);
+    eventList.results[i].EVENT_END = convertDateToClientTimezone(eventList.results[i].EVENT_END, clientTimezone);
+  }
+  return eventList;
+}
 
