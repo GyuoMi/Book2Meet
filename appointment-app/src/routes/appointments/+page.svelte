74372258cfaf442aa9709f0d3022bd44390e0a0a
Sahil Dinanath <sahildinanath@gmail.com>
2023-05-12 16:31:33 +0200

    integration of ui for autocomplete search bar

--- appointment-app/src/routes/appointments/+page.svelte
+++ appointment-app/src/routes/appointments/+page.svelte
@@ -4,6 +4,8 @@
 	import '@event-calendar/core/index.css';
 	import Interaction from '@event-calendar/interaction';
 	import { deleteEventFromCalender, getArrayOfEventsFromDatabase } from '../schedule/page.modules';
+  import autoComplete from './autoCompleteStyle.svelte';
+
 	/** @type {import('./$types').ActionData} */
 	export let form;
 
@@ -87,6 +89,64 @@
     console.log(allUserBookingsJson);
   }
 
+//from this point the code is used to handel the suggested searches
+let filteredSearches = [];
+
+const filterSearch = () => {
+	let storageArr = []
+	if (inputValue) {
+		countries.forEach(country => {
+			 if (country.toLowerCase().startsWith(inputValue.toLowerCase())) {
+				 storageArr = [...storageArr, makeMatchBold(country)];
+			 }
+		});
+	}
+	filteredSearches = storageArr;
+}	
+
+let searchInput; // use with bind:this to focus element
+let inputValue = "";
+	
+$: if (!inputValue) {
+	filteredSearches = [];
+	hiLiteIndex = null;
+}
+const clearInput = () => {
+	inputValue = "";	
+	searchInput.focus();
+}
+const setInputVal = (email) => {
+	inputValue = removeBold(email);
+	filteredSearches = [];
+	hiLiteIndex = null;
+	document.querySelector('#country-input').focus();
+}	
+const makeMatchBold = (str) => {
+	// replace part of (country name === inputValue) with strong tags
+	let matched = str.substring(0, inputValue.length);
+	let makeBold = `<strong>${matched}</strong>`;
+	let boldedMatch = str.replace(matched, makeBold);
+	return boldedMatch;
+}
+const removeBold = (str) => {
+	//replace < and > all characters between
+	return str.replace(/<(.)*?>/g, "");
+	// return str.replace(/<(strong)>/g, "").replace(/<\/(strong)>/g, "");
+}	/* NAVIGATING OVER THE LIST OF COUNTRIES W HIGHLIGHTING */	
+let hiLiteIndex = null;
+//$: console.log(hiLiteIndex);	
+$: hiLitedCountry = filteredSearches[hiLiteIndex]; 	
+const navigateList = (e) => {
+	if (e.key === "ArrowDown" && hiLiteIndex <= filteredSearches.length-1) {
+		hiLiteIndex === null ? hiLiteIndex = 0 : hiLiteIndex += 1
+	} else if (e.key === "ArrowUp" && hiLiteIndex !== null) {
+		hiLiteIndex === 0 ? hiLiteIndex = filteredSearches.length-1 : hiLiteIndex -= 1
+	} else if (e.key === "Enter") {
+		setInputVal(filteredSearches[hiLiteIndex]);
+	} else {
+		return;
+	}
+} 
 </script>
 
 <form method="POST" action="?/getSearchedEmailEvents">
@@ -118,4 +178,61 @@
 		>
 <input type="hidden" name="userBookings" bind:value={allUserBookingsJson} />
 			</div>
+</form> 
+  <svelte:window on:keydown={navigateList} />
+
+<form autocomplete="off" on:submit|preventDefault={submitValue}>
+  <div class="autocomplete">
+    <input id="country-input" 
+					 type="text" 
+					 placeholder="Search Country Names" 
+					 bind:this={searchInput}
+					 bind:value={inputValue} 
+					 on:input={filterSearch}>
+  </div>
+	
+  <input type="submit">
+	
+	<!-- FILTERED LIST OF COUNTRIES -->
+	{#if filteredSearches.length > 0}
+		<ul id="autocomplete-items-list">
+			{#each filteredSearches as country, i}
+				<Country itemLabel={country} highlighted={i === hiLiteIndex} on:click={() => setInputVal(country)} />
+			{/each}			
+		</ul>
+	{/if}
 </form>
+<!-- used to style the search bar  -->
+<style>
+div.autocomplete {
+  /*the container must be positioned relative:*/
+  position: relative;
+  display: inline-block;
+	width: 300px;
+}
+input {
+  border: 1px solid transparent;
+  background-color: #f1f1f1;
+  padding: 10px;
+  font-size: 16px;
+	margin: 0;
+}
+input[type=text] {
+  background-color: #f1f1f1;
+  width: 100%;
+}
+input[type=submit] {
+  background-color: DodgerBlue;
+  color: #fff;
+}
+	
+#autocomplete-items-list {
+	position: relative;
+	margin: 0;
+	padding: 0;
+	top: 0;
+	width: 297px;
+	border: 1px solid #ddd;
+	background-color: #ddd;
+}	
+</style>	
