ac60b05859b9650c3ea5c8a41c080dfd6899954c
Sahil Dinanath <sahildinanath@gmail.com>
2023-05-23 20:03:36 +0200

    removed analytics page

--- appointment-app/src/routes/analytics/svelte-frappe-charts.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'svelte-frappe-charts' {
-    import { SvelteComponent } from 'svelte';
-  
-    export default class Chart extends SvelteComponent {
-      constructor(options: any);
-    }
-  }
-  
-  
