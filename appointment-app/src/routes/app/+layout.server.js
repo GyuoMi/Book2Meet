10e339d0d3b3c8092748b28a4dc7bf06d230f625
Ilona Smith <smithilona247@gmail.com>
2023-05-26 20:49:54 +0200

    fix

--- appointment-app/src/routes/app/+layout.server.js
+++ appointment-app/src/routes/app/+layout.server.js
@@ -4,18 +4,25 @@ import {_clientID} from '../login/+page.server.js';
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
   let bookingDetailsFromDatbaseJson;
+  let allBookings;
 //checks if clientID is defined before trying to access database with this value
 //if client has not logged in yet return an empty json
   if (_clientID !== undefined) {
     bookingDetailsFromDatbaseJson = await database.getJsonFromSelectQuery(
       `select * from BOOKING_TBL where CLIENT_ID = ${_clientID} AND EVENT_START > NOW()`
     );
+    allBookings = await database.getJsonFromSelectQuery(
+      `select * from BOOKING_TBL where EVENT_START > NOW()`
+    );
   } else {
     bookingDetailsFromDatbaseJson = { results: [] };
+    allBookings = { results: [] };
   }
 
   console.log(bookingDetailsFromDatbaseJson);
+  console.log(allBookings);
   return {
-    post: bookingDetailsFromDatbaseJson
+    bookingDetails: bookingDetailsFromDatbaseJson,
+    allBookings: allBookings
   };
 }
