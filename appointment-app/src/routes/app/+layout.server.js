fc66cb7296bc130ffc2c15f5e69e4c5a40b431b9
Darshan Singh <88923579+GyuoMi@users.noreply.github.com>
2023-05-28 21:51:25 +0200

    Update +layout.server.js
    fixed bookingDetailsFromDatabaseJson

--- appointment-app/src/routes/app/+layout.server.js
+++ appointment-app/src/routes/app/+layout.server.js
@@ -3,12 +3,12 @@ import {_clientID} from '../login/+page.server.js';
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
-  let bookingDetailsFromDatbaseJson;
+  let bookingDetailsFromDatabaseJson;
   let allBookings;
 //checks if clientID is defined before trying to access database with this value
 //if client has not logged in yet return an empty json
   if (_clientID !== undefined) {
-    bookingDetailsFromDatbaseJson = await database.getJsonFromSelectQuery(
+    bookingDetailsFromDatabaseJson = await database.getJsonFromSelectQuery(
       `select B.*, C.CLIENT_FIRST_NAME, C.CLIENT_LAST_NAME from BOOKING_TBL B 
       JOIN(SELECT E.EVENT_ID, E.CLIENT_ID, C.CLIENT_FIRST_NAME, C.CLIENT_LAST_NAME FROM EVENT_TBL E JOIN CLIENT_TBL C 
       ON E.CLIENT_ID = C.CLIENT_ID) AS C ON B.EVENT_ID = C.EVENT_ID where B.CLIENT_ID = ${_clientID} AND B.EVENT_START > NOW()`
@@ -17,14 +17,14 @@ export async function load() {
       `select * from BOOKING_TBL where EVENT_START > NOW()`
     );
   } else {
-    bookingDetailsFromDatbaseJson = { results: [] };
+    bookingDetailsFromDatabaseJson = { results: [] };
     allBookings = { results: [] };
   }
 
-  console.log(bookingDetailsFromDatbaseJson);
+  console.log(bookingDetailsFromDatabaseJson);
   console.log(allBookings);
   return {
-    bookingDetails: bookingDetailsFromDatbaseJson,
+    bookingDetails: bookingDetailsFromDatabaseJson,
     allBookings: allBookings
   };
 }
